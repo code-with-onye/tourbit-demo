ab42f03a67d4d4d129bfafc993e52431525a55a5
Trust Onyemachi <85189857+code-with-onye@users.noreply.github.com>
2024-09-21 20:34:21 +0100

    features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,13 +78,12 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"  id="feature3">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-          id="feature3"
         >
           <Image
             aria-hidden
